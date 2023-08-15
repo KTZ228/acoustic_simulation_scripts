8905007b2c71f902f888a0b5f8ae013717da1161
KTZ228 <kennethvdzee@gmail.com>
2023-08-15 15:49:17 +0200

    Changed filenames and added additional comments to scripts

--- create_group_MNI_plots_call.m
+++ create_group_MNI_plots_call.m
@@ -5,21 +5,21 @@ cd /home/mrphys/kenvdzee/Documents/PRESTUS
 addpath('functions')
 addpath(genpath('toolboxes')) 
 addpath('/home/common/matlab/fieldtrip/qsub')
-masks_location = '/project/3023001.06/Simulations/';
+masks_location = '/project/3023001.06/Simulations/kenneth_test/simulations/ROI_masks/';
 
-parameters = load_parameters('sjoerd_config_opt_CTX250-026_105_61.5mm.yaml');
-parameters.results_filename_affix = '_target_right_amygdala';
+parameters = load_parameters('config_kenneth_phd_1_amygdala_posthoc_CTX250-001_203_60.9mm.yaml');
+parameters.results_filename_affix = '_target_left_amygdala';
 
 % Extract list of participants
-%files = struct2table(dir(parameters.data_path));
-%subject_list_table = files(logical(contains(files.name, 'sub') .* ~contains(files.name, 'm2m')),:);
-%subject_list = str2double((extract(subject_list_table{:,1}, digitsPattern))');
-subject_list = [1,3,4,5,8,9,10,14,17,18,19];
-%subject_list = 1;
+files = struct2table(dir(parameters.data_path));
+subject_list_table = files(logical(contains(files.name, 'sub') .* ~contains(files.name, 'm2m')),:);
+subject_list = str2double((extract(subject_list_table{:,1}, digitsPattern))');
+subject_list = [3,4,5];
 
 % Load ROI
-maskname = 'juelich_prob_GM_Amygdala_laterobasal_groupL_thr75_bin.nii.gz';
+maskname = 'juelich_prob_GM_Amygdala_laterobasal_groupR_thr75_bin.nii.gz';
 mask_location = fullfile(masks_location, maskname);
 mask = niftiread(mask_location);
 
-create_group_MNI_plots(subject_list, parameters, 'ROI_MNI_mask', mask, 'outputs_suffix', '_max_intensity', 'plot_max_intensity', 1, 'add_FWHM_boundary', 1, 'plot_heating', 0, 'brightness_correction', 1)
+create_group_MNI_plots(subject_list, parameters, 'ROI_MNI_mask', mask, 'outputs_suffix', '_max_intensity', 'plot_max_intensity', 1,...
+    'add_FWHM_boundary', 1, 'plot_heating', 0, 'brightness_correction', 1)
